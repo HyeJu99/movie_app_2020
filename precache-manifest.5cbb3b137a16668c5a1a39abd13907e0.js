3add6672c996c0517eb837526124bf10406b4d52
Hyeju99 <sinhye102@gmail.com>
2020-07-30 14:12:12 +0900

    Updates

---
+++ precache-manifest.5cbb3b137a16668c5a1a39abd13907e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c56a579cad7b558f2f3ea1b5853103",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "78d3a89317e6934c587c",
+    "url": "/movie_app_2020/static/css/main.35669eb3.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d3a89317e6934c587c",
+    "url": "/movie_app_2020/static/js/main.9823c2a4.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
